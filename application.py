00164e4f9d8b60789a0478ea62ab128920a2de0d
= <=>
2025-06-03 22:04:22 +0530

    add

--- application.py
+++ application.py
@@ -34,8 +34,6 @@ def predict_datapoint():
         return render_template('home.html',results=result[0])
     
     return render_template('home.html')
-    
-
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
